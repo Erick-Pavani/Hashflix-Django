d875799545dc30f873a8238a638ce35410982873
Erick-Pavani <erickspavanii@gmail.com>
2022-06-29 00:55:06 -0300

    Added tailwind and bootstrap

---
+++ filme/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+LISTA_CATEGORIAS = (
+    ("ANALISES", "Análises"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+    ("OUTROS", "Outros"),
+)
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to="thumb_filmes")
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.id} - {self.titulo}"
+
