4f8c4d902202be9bf057d8ba4b8f43d9b5f8a405
Erick-Pavani <erickspavanii@gmail.com>
2022-07-06 00:54:51 -0300

    Homepage testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html',
-    './node_modules/flowbite/**/*.js'
+    './templates/*.html',
+    './filme/templates/*.html',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
